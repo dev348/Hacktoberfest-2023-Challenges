5931c0a228b84f8d73a19db2b210668a96d1a18f
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-09-26 23:12:25 +0530

    project initialization

---
+++ api/app.js
@@ -0,0 +1,15 @@
+/*  
+Welcome to the REST API Challenge for Hacktoberfest 2023, Created by the FOSS Community of NSBM.
+
+In this Challenge, you have the creative freedom to design your `app.js` file as you see fit. 
+Express.js is a versatile framework, and you can structure your application in various ways 
+to suit your specific needs.
+
+Feel free to organize your code, create additional files/modules/directories, or use any libraries 
+you find useful to build your API.
+
+The structure and functionality of this will depend on the approach that you try to solve the 
+challenge,so make it your own and have fun coding!
+
+Happy coding! 😊
+*/
